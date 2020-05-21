4fb046b15888a38cbd1e33fff797a72da81bb754
parikshit singh <pk223933@gmail.com>
2020-05-21 18:01:33 +0530

    fixed smooth scroll

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 "use strict";
-function add_listener_to_the_element(itr, y_pos, amount_of_scroll)
+function add_listener_to_the_element(itr, amount_of_scroll)
 {
-    itr.addEventListener('click', function ()
+    itr.addEventListener('click', function (event)
     {
+        event.preventDefault();
+        let position_of_element_from_top = document.getElementById(itr.innerText.toLowerCase()).getBoundingClientRect().y;
         var id = setInterval(function ()
         {
             window.scrollBy(0, amount_of_scroll);/* here the second entry is the amount by which i want to scroll */
-            if (window.pageYOffset >= y_pos) {
+            console.log(window.pageYOffset, position_of_element_from_top);
+            if (window.pageYOffset >= position_of_element_from_top) {
                 clearInterval(id);
+                return;
             }
         }, 1)/* this is the time after which it will scroll */
     })
@@ -18,21 +22,21 @@ for (let itr of list) {
         continue;
     }
     else if (itr.innerText == "About") {
-        add_listener_to_the_element(itr, 400, 10);
+        add_listener_to_the_element(itr, 10);
     }
     else if (itr.innerText == "Skills") {
-        add_listener_to_the_element(itr, 1000, 20);
+        add_listener_to_the_element(itr, 20);
     }
     else if (itr.innerText == "Experience") {
-        add_listener_to_the_element(itr, 2000, 20);
+        add_listener_to_the_element(itr, 20);
     }
     else if (itr.innerText == "Education") {
-        add_listener_to_the_element(itr, 2500, 20);
+        add_listener_to_the_element(itr, 20);
     }
     else if (itr.innerText == "Portfolio") {
-        add_listener_to_the_element(itr, 4000, 25);
+        add_listener_to_the_element(itr, 25);
     }
     else if (itr.innerText == "Contact") {
-        add_listener_to_the_element(itr, 4558, 40);
+        add_listener_to_the_element(itr, 40);
     }
 }
